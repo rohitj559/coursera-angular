07c669e641f803d7672797fb46e58feb2afc53f5
rohit <rohitj559@gmail.com>
2019-08-11 08:08:29 -0500

    Conversion  to promises using observables

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,17 +12,14 @@ export class LeaderService {
   constructor() { }
 
   getLeaders(): Promise<Leader[]> {
-    return new Promise(resolve =>
-      setTimeout(() => resolve(LEADERS), 2000));
+    return of(LEADERS).pipe(delay(2000)).toPromise();
   }
 
   getLeader(id: string): Promise<Leader> {
-    return new Promise(resolve =>
-      setTimeout(() => resolve(LEADERS.filter(lead => lead.id === id)[0]), 2000));
+    return of(LEADERS.filter((lead) => (lead.id === id))[0]).pipe(delay(2000)).toPromise();
   }
 
   getFeaturedLeader(): Promise<Leader> {
-    return new Promise(resolve =>
-      setTimeout(() => resolve(LEADERS.filter(lead => lead.featured)[0]), 2000));
+    return of(LEADERS.filter(lead => lead.featured)[0]).pipe(delay(2000)).toPromise();
   }
 }
